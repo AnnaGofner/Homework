93e874eba391bfbc43d478bbfbb8e9496048535b
AnnaGofner <annushkamaslovna@gmail.com>
2021-11-04 02:22:06 +0300

    New hw

--- 1.11.2.py
+++
@@ -1,6 +0,0 @@
-time_str = input("Введите время в секундах: ")
-time = int(time_str)
-hours = time // 3600
-minutes = (time - hours * 3600) // 60
-seconds = ((time - hours * 3600) - minutes * 60) // 60
-print(f"Time: {hours:02}:{minutes:02}:{seconds:02}")
