9a2a97dac440d367f2ea9cccdac3870d663cccbf
AnnaGofner <annushkamaslovna@gmail.com>
2021-11-06 00:04:13 +0300

    Homework 1

--- 1.11.3.py
+++
@@ -1,8 +0,0 @@
-# number_str = input("Print a number: ")
-# number = int(number_str)
-# print(number + (number*11) + (number*111))
-
-user_number = input("Print a number: ")
-n = int(user_number)
-number = (n + int(str(n) + str(n)) + int(str(n)+str(n)+str(n)))
-print(number)
